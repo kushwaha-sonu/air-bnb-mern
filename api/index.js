088a139cc440fc2e92613773203442513d18cbe1
Sonu kumar <104054249+kushwaha-sonu@users.noreply.github.com>
2024-06-24 11:17:16 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -50,7 +50,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'https://sonu-booking-website.netlify.app',
+    origin: 'https://air-bnb-mern-fontend.onrender.com',
 }));
 
 app.use((req, res, next) => {
